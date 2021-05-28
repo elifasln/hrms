556019a50d2236ab98ab7fbda98b09294db9b31c
Elif Aslan <elifaslann5534@gmail.com>
2021-05-29 01:09:04 +0300

    Update HrmsApplication.java

--- hrms/src/main/java/kodlamaiocamp/hrms/HrmsApplication.java
+++ hrms/src/main/java/kodlamaiocamp/hrms/HrmsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HrmsApplication {
-
+//asdasd
 	public static void main(String[] args) {
 		SpringApplication.run(HrmsApplication.class, args);
 	}
